d642f421e7329b3b1340de0f0c2b308a8d5e9d03
frankrodrib-cyber <frankrodrib@gmail.com>
2026-01-06 12:16:40 -0500

    Estructura final corregida para Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import OroJuezApp from './App.jsx'
-import './style.css'
+import './style.css' // Esto busca el archivo en la misma carpeta src
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
